b6d87e4692750072355b1ce2668d936b804db16f
Lucas-sandrade <lucas__sandrade@outlook.com>
2023-10-09 13:19:53 -0300

    Adicionando js a tela princial do projeto

---
+++ PWNB_ENTREGAS/PWNB-Semana#06/home.js
@@ -0,0 +1,21 @@
+function scrollToTools() {
+    const ferramentasSection = document.getElementById('cards-menu');
+    if (ferramentasSection) {
+      ferramentasSection.scrollIntoView({ behavior: 'smooth' }); // Rola suavemente para a seção
+    } else {
+      console.error("Elemento com ID 'ferramentas' não encontrado.");
+    }
+  }
+  
+  function moveDescSection() {
+    const infoSection = document.querySelector('.desc-txt');
+    if (infoSection) {
+        infoSection.classList.add('shake'); // Adicione a classe 'shake' para ativar a animação
+        setTimeout(() => {
+          infoSection.classList.remove('shake'); // Remova a classe 'shake' após a animação terminar
+        }, 500);
+    } else {
+      console.error("Elemento com classe 'desc' não encontrado.");
+    }
+  }
+  
